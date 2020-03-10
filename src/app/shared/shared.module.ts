d652f6e1abddfebb3aa69fd41c9d6e3330e814de
Venkatesh Arepalli <36284688+venkatesharepalli@users.noreply.github.com>
2020-03-10 22:23:31 +0100

    Added components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,5 @@
 import {NgModule} from "@angular/core";
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ChartsModule } from 'ng2-charts';
 import {CustomMaterialModule} from "../core/material.module";
@@ -7,25 +8,32 @@ import {
 } from './table-list/table-list.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
+import { DropDownComponent } from './drop-down/drop-down.component';
+import { ToggleButtonGroupComponent } from './toggle-button-group/toggle-button-group.component';
 
 
 @NgModule({
   declarations: [
     TableListComponent,
     BarChartComponent,
-    ErrorMessageComponent
+    ErrorMessageComponent,
+    DropDownComponent,
+    ToggleButtonGroupComponent
   ],
   imports: [
     CommonModule,
     CustomMaterialModule,
-    ChartsModule
+    ChartsModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
     CustomMaterialModule,
     TableListComponent,
     BarChartComponent,
-    ErrorMessageComponent
+    ErrorMessageComponent,
+    DropDownComponent,
+    ToggleButtonGroupComponent
   ],
 })
 export class SharedModule { }
